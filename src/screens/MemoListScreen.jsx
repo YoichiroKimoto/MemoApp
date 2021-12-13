dcbdb8c615db3522ba9ae40dc26dd56d0970ba55
YoichiroKimoto <64562711+YoichiroKimoto@users.noreply.github.com>
2021-12-13 13:01:11 +0900

    Pass it to FlatList

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Alert } from 'react-native';
 import firebase from 'firebase';
 
@@ -8,6 +8,7 @@ import LogOutButton from '../components/LogOutButton';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+  const [memos, setMemos] = useState([]);
   useEffect(() => {
     navigation.setOptions({
       // eslint-disable-next-line react/no-unstable-nested-components
@@ -22,9 +23,17 @@ export default function MemoListScreen(props) {
     if (currentUser) {
       const ref = db.collection(`users/${currentUser.uid}/memos`).orderBy('updatedAt', 'desc');
       unsbscribe = ref.onSnapshot((snapshot) => {
+        const userMemos = [];
         snapshot.forEach((doc) => {
           console.log(doc.id, doc.data());
+          const data = doc.data();
+          userMemos.push({
+            id: doc.id,
+            bodyText: data.bodyText,
+            updatedAt: data.updatedAt.toDate(),
+          });
         });
+        setMemos(userMemos);
       }, (error) => {
         console.log(error);
         Alert.alert('データの読み込みに失敗しました');
@@ -34,7 +43,7 @@ export default function MemoListScreen(props) {
   }, []);
   return (
     <View style={styles.container}>
-      <MemoList />
+      <MemoList memos={memos} />
       <CircleButton
         name="plus"
         onPress={() => { navigation.navigate('MemoCreate'); }}
